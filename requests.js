20fd7902d93b5f3790baefedfbf91ac809b480bc
RealSebasTheFish <sebastianq04@gmail.com>
2025-01-24 19:10:51 -0500

    basic testing

---
+++ requests.js
@@ -0,0 +1,26 @@
+async function getRequest (url, data) {
+    return new Promise((resolve, reject) => {
+        // Create a new XMLHttpRequest object and store it in a variable called xhr
+        const xhr = new XMLHttpRequest();
+        
+        /* Specified the request type (GET) and the endpoint where the request will be sent to, in this 
+        case get disaster reports to get the number of disasters reported*/
+        xhr.open("GET", url);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+        //Send the request to the server
+        xhr.send(JSON.stringify(data));
+
+        //Ensure answer is received in json format
+        xhr.responseType = "json";
+
+        //Onload event listener to receive the server's response
+        xhr.onload = () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+            resolve(data);
+        } else {
+            reject(xhr.status);
+        }
+        };
+    });
+}
